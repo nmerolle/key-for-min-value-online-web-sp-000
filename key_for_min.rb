b662fe90d27c4291f02a1624222a9a3afe6ca862
Nicholas Merolle <nmerolle11@hotmail.com>
2020-09-09 02:09:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,5 +5,7 @@ def key_for_min_value(name_hash)
   default = 0
   lowest =  nil
   name_hash.each do |key, value|
-    if default == 0||     
+    if default == 0|| value < default
+      lowest = value
+      
 end
